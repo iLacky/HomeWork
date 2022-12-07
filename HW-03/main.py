8dba0bdb1214ee350de8d91de0a08d7b2052a0c2
iLacky <119632778+iLacky@users.noreply.github.com>
2022-12-07 22:06:15 +0100

    'HW3'

---
+++ HW-03/main.py
@@ -0,0 +1,33 @@
+text = 'I love Python '           
+text *= 42
+print(text)
+
+
+one_month_of_the_year = 10 / 12   # Для вирахування  значення одного місяця в році
+age = 23                         
+month_of_year = 12               
+remaining_months = 5             
+age_in_month = age * month_of_year +(remaining_months * one_month_of_the_year)
+print(age_in_month)
+
+age_in_years = age
+print(age_in_years)
+
+age_in_years = str(age)
+name = 'Volodymyr'
+my_age = 'My name is ' + (name) +' I`m ' + (age_in_years) + ' years old.'
+print(my_age)
+
+number = 1
+print (number > 2)
+print (number < 5)
+print (number == 3)
+print (number != 4)
+print (number <= 6)
+
+
+a = 2
+b = 5
+c = 6
+d = str(a) + str(b) + str(c)
+print (d)
