da747baa89483fdf2871f91aba0930f370316a57
iLacky <119632778+iLacky@users.noreply.github.com>
2022-12-12 09:29:01 +0100

    Update hw4.py
    removing functions from code

--- HW-04/hw4.py
+++ HW-04/hw4.py
@@ -3,13 +3,11 @@ if text.isdigit():
     text = int(text)
     print(str(text)+' it`s numeric.')
     print(type(text))
-    def is_even(text):
-        return text % 2 == 0
+text % 2 == 0
     if is_even(text):
         print(str(text) + ' it`s even.')
     else:
         print(str(text) + ' it`s add.')
-
 else:
     print(str(text)+' it`s word.')
     print(type(text))
