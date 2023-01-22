4448296210e1305bdcf98ccbd68da6f98cbf25e0
iLacky <119632778+iLacky@users.noreply.github.com>
2023-01-22 19:55:52 +0100

    Update homework11(2).py

--- HW-11/homework11(2).py
+++ HW-11/homework11(2).py
@@ -1,12 +1,10 @@
-import json
 from datetime import datetime
 
 
 class MyCustomException(Exception):
     time = datetime.now()
     exc = f'{Exception} Time: {time}'
-    with open(r'C:\HW\HomeWork\HW-11\file_exc', 'x') as file_exc:
-        exc = json.dumps(exc)
+    with open('file_exc', 'w+') as file_exc:
         file_exc.write(exc)
 
 if True:
