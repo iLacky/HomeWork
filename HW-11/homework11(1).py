690f3abdd6f3d6a7cd939bb9202abba788436a3b
iLacky <119632778+iLacky@users.noreply.github.com>
2023-01-22 19:56:14 +0100

    Update homework11(1).py

--- HW-11/homework11(1).py
+++ HW-11/homework11(1).py
@@ -1,15 +1,14 @@
-import json
 from datetime import datetime
 
 
 def MyDecFunc(func):
-    def wrap():
+    def wrap(**kwargs):
         time = datetime.now()
         text = f'Open function {func} \n Time: {time}'
-        with open(r'C:\HW\HomeWork\HW-11\file_dec', 'x') as file_dec:
-            text = json.dumps(text)
+        with open('file_dec', 'x') as file_dec:
             file_dec.write(text)
-        func()
+        func(**kwargs)
+        return func
     return wrap
 
 
