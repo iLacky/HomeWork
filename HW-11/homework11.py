416e69a6230206170e6dfd90894bb7b4e3862868
iLacky <119632778+iLacky@users.noreply.github.com>
2023-01-22 19:55:15 +0100

    Update homework11.py

--- HW-11/homework11.py
+++ HW-11/homework11.py
@@ -6,20 +6,15 @@ data = {
 
 }
 
-
-try:
-    with open(r'C:\HW\HomeWork\HW-11\file', 'x') as file:
-        data = json.dumps(data)
-        file.write(data)
-        print('Open file')
-except FileExistsError:
+with open(r'C:\HW\HomeWork\HW-11\file', 'w+') as file:
+    data = json.dumps(data)
+    file.write(data)
     print('Open file')
+with open(r'C:\HW\HomeWork\HW-11\file', 'r+') as file:
+    file_dict = file.read()
+    file_dict = json.loads(file_dict)
 
 while True:
-    with open(r'C:\HW\HomeWork\HW-11\file', 'r+') as file:
-        file_dict = file.read()
-        file_dict = json.loads(file_dict)
-
     contacts = input('Enter your contact:')
     if contacts == 'list':
         print(file_dict)
@@ -28,23 +23,31 @@ while True:
     elif contacts == 'add':
         new_contacts = input('Enter new contact: ')
         key = new_contacts
+        with open(r'C:\HW\HomeWork\HW-11\file', 'r+') as file:
+            file_dict = file.read()
+            file_dict = json.loads(file_dict)
         if key in file_dict:
             print(f'This contact already exist.')
         else:
             number = input('Enter number: ')
             num = int(number)
             file_dict[key] = num
-            with open(r'C:\HW\HomeWork\HW-11\file', 'w') as file:
+            with open(r'C:\HW\HomeWork\HW-11\file', 'w+') as file:
                 file_dict = json.dumps(file_dict)
                 file.write(file_dict)
+                file_dict = json.loads(file_dict)
             print(f'Add new contact: {key} {num}')
     elif contacts == 'delete':
         del_con = input('Enter the name of the contact you want to delete: ')
+        with open(r'C:\HW\HomeWork\HW-11\file', 'r+') as file:
+            file_dict = file.read()
+            file_dict = json.loads(file_dict)
         if del_con in file_dict:
             del file_dict[del_con]
-            with open(r'C:\HW\HomeWork\HW-11\file', 'w') as file:
+            with open(r'C:\HW\HomeWork\HW-11\file', 'w+') as file:
                 file_dict = json.dumps(file_dict)
                 file.write(file_dict)
+                file_dict = json.loads(file_dict)
             print(f'Contact delete.')
         else:
             print('This contact does not exist')
