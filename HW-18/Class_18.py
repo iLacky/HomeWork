48efd5952965eb2d4d181b27e90dd2404d255110
iLacky <119632778+iLacky@users.noreply.github.com>
2023-02-06 21:34:14 +0100

    'new_homework'

---
+++ HW-18/Class_18.py
@@ -0,0 +1,34 @@
+class Bot:
+    def __init__(self, name):
+        self.name = name
+    def say_message(self, message):
+        print(message)
+    def say_name(self,):
+        print(self.name)
+
+
+obj = Bot('Marvin')
+obj.say_name()
+obj.say_message('message')
+
+
+class TelegramBot(Bot):
+    url = None
+    chat_id = None
+    def __init__(self, name):
+        Bot.__init__(self, name)
+
+    def set_url(self, url):
+        self.url = url
+        print(url)
+    def set_chat_id(self, chat_id):
+        self.chat_id = chat_id
+        return chat_id
+    def send_message(self, message):
+        print(f'{self.name} bot says {message} to chat {self.chat_id} using {self.url}')
+
+
+telegram_bot = TelegramBot('TG')
+telegram_bot.say_name()
+telegram_bot.set_url(1)
+telegram_bot.send_message('Hello')
