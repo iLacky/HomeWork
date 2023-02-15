d43fd583af1b833e9f99c7f65e3721dc46e5378f
iLacky <119632778+iLacky@users.noreply.github.com>
2023-02-15 13:25:48 +0100

    'home_work_SQL'

---
+++ HW-20/SQL(20.2).py
@@ -0,0 +1,5 @@
+INSERT INTO users(first_name, last_name, age) VALUES ('Homer', 'Simpson', 73);
+INSERT INTO users(first_name, last_name, age) VALUES ('Marge', 'Simpson', 72);
+INSERT INTO users(first_name, last_name, age) VALUES ('Bart', 'Simpson', 44);
+INSERT INTO users(first_name, last_name, age) VALUES ('Lisa', 'Simpson', 40);
+INSERT INTO users(first_name, last_name, age) VALUES ('Maggie', 'Simpson', 37)
