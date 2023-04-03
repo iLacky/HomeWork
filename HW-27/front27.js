672ee0b53de3932f70c98d4cdbc3ee486f685f01
iLacky <119632778+iLacky@users.noreply.github.com>
2023-04-03 12:13:57 +0200

    Update front27.js

--- HW-27/front27.js
+++ HW-27/front27.js
@@ -1,19 +1,19 @@
 let int = document.getElementById("demo").innerHTML = Math.floor(Math.random() * 1000);
 let i = 1
 
-const myDiv = document.createElement('button');
-myDiv.className = 'buttons';
-myDiv.style.textAlign = 'center';
+const myBut = document.createElement('button');
+myBut.className = 'buttons';
+myBut.style.textAlign = 'center';
 
 
-['Add friends', 'Send message', 'Offer a job'].map(buttonName => {
-    let button = document.createElement('button');
+['Add friends'].map(butonName => {
+    let button = document.createElement('buton');
     button.className = 'btn btn-success';
-    button.innerText = buttonName;
+    button.innerText = butonName;
     button.style.margin = '5px';
-    myDiv.appendChild(button)
+    myBut.appendChild(button)
 })
-document.getElementsByTagName('body')[0].appendChild(myDiv)
+document.getElementsByTagName('body')[0].appendChild(myBut)
 
 const btn = document.getElementsByTagName('button')[0];
 btn.addEventListener('click' ,(myButEvent) =>{
